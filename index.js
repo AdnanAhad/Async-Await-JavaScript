2cf49e9d9226a93d377ddcec2b9e50dd0138dcb6
Adnan Ahad <adnanahad.in@gmail.com>
2021-11-20 06:22:28 +0000

    lesson completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("first line");
+console.log("first line"); // 1
+
+async function demo() {
+  console.log("Indide function"); // 3
+
+  const promise = await fetch("https://api.github.com/users");
+  console.log("after promise"); // 7
+  const users = await promise.json();
+  console.log("after users"); // 8
+  return users;
+}
+
+console.log("before calling function "); // 2
+const demoCall = demo();
+console.log("after calling function"); //4
+console.log(demoCall); //5
+
+demoCall.then((data) => console.log(data)); //9
+
+console.log("last line of code"); //6
+
+// -------------------------------------------------------
+
+// console.log("first line of code"); //1
+// function demo2() {
+//   console.log("inside function");
+//   fetch("https://api.github.com/users")
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+//   console.log("after fetch");
+//   return "returning value";
+// }
+
+// console.log("before calling");
+// const demoCall2 = demo2();
+// console.log("after calling");
+// console.log(demoCall2);
+// console.log("last Code");
